c3aa0761b30006ca2a76b0e90a30ac39b0ef7c1e
coinZee <coinz1112@gmail.com>
2024-01-12 10:35:38 +0000

    yes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Discord-terminal
-basically ur system terminal in discord
+Basically ur system terminal but control in discord
 
 # Purpose?
 
@@ -18,21 +18,29 @@ Please exercise caution when using this bot as it provides direct access to the 
 ## Installation
 
 1. Clone the repository: `git clone https://github.com/coinZee/discord-terminal.git`
+    - Check if you have python installed with ```python --version```
+    - If ```python: command not found``` install it first 
+
 2. Install the required dependencies: `pip install -r requirements.txt`
 3. Configure the bot token in the `.env` file.
     - Replace the BOT_KEY, Run the bot and use /show_id command it'll show you your id
     - You dont need to put "" or '' around the token and id
     - Replace the OWNER_ID with your id
 4. Start the bot: `python main.py`
+    - Or if u want the bot to be running in the background, ```python main.py &```. And then use ```disown```. u can close the terminal.
+5. Closing the bot
+    - You can use ```htop``` and kill the process
+    - Alternatively you can use pgrep and pkill ```999 (with actual pid)```
 
 ## Usage
 
 - Invite the bot to your server, type in any channel the bot has access to, the command will run.
+
 - /ctrl_c to kill current process
 
 ## Contributing
 
-Contributions are welcome! If you have any suggestions, bug reports, or feature requests, please open an issue or submit a pull request.
+Relative small and starting point project but contributions are welcome! If you have any suggestions, bug reports, or feature requests, please open an issue or submit a pull request.
 
 ## License
 
--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import discord
 from dotenv import load_dotenv
 import subprocess
 from discord.ext import commands
+import asyncio
 
 load_dotenv()
 
+########################
+# Bot setup starts here
+########################
+
 BOT_KEY=os.getenv('BOT_KEY')
+OWNER_ID=os.getenv('OWNER_ID')
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix = "/", intents=intents)
@@ -17,43 +23,87 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(name="Your PC!!"))
     print('Logged in as {0.user}'.format(bot))
 
+#######################
+# Bot setup ends here
+#######################
+
 global process  # Declare process as a global variable
 process = None  # Set process to None
 
+# Main function to run the command
 def run_command(command: str):
     global process  # Access the global process variable
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-    output = output.decode().strip()
+
+    # Start the process with given command
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
+    output, error = process.communicate() # Get the output and error of the process
+    output = output.decode().strip() 
     if not output:
         output = "No output"
-        process.kill()
+        process.kill() # Kills the process if there is no output
         return
-
-    process.kill()
+    
+    process.kill() # Kills the process after getting the output
     return output
 
+# Command to kill process
 @bot.hybrid_command()
+@commands.is_owner()
 async def ctrl_c(ctx):
     global process  # Access the global process variable
+    # Checks if there any processes to kill
     if process == None:
         await ctx.send('No process to kill')
         return
+    # If any process to kill, then kills it and send the 'process id' is killed
     process.kill()
-    await ctx.send(f'{process.pid} killed')
+    await ctx.send(f'``` {process.pid} killed ```')
 
+@ctrl_c.error
+async def ctrl_c_error(ctx, error):
+    if isinstance(error, commands.NotOwner):
+        await ctx.send("You dont have access to do that <3", ephemeral=True)
+
+# Function that happpens every time a message is sent
 @bot.event
+@commands.is_owner()
 async def on_message(message):
+
+    # Check if the message author is the bot itself
     if message.author == bot.user:
         return
+    
+    # Check if the message starts with the prefix
     if message.content.startswith("/"):
         await bot.process_commands(message)
         return
-    await message.add_reaction('✅')
-    response = run_command(str(message.content).lower().strip())
-    if response == "":
-        response = "No output"
-    await message.channel.send(f"``` {response} ```")
-    await message.remove_reaction('✅', bot.user)
-        
+    
+    # Check if the message author is the bot owner
+    if str(message.author.id) != OWNER_ID:
+        #await message.channel.send(message.author.id, ephemeral=True)
+        #await message.channel.send(bot.owner_id, ephemeral=True)
+        # Code for the none-bot owner
+        await message.add_reaction('❌')
+        #await message.channel.send("You cant use this bot", ephemeral=True) # Tells them they are not the owner u can uncommand this line if u want
+        await asyncio.sleep(3)
+        await message.remove_reaction('❌', bot.user)
+        return
+
+    else:
+        # Code for owner 
+        await message.add_reaction('✅')
+        # Calls the function with what user gives as input
+        response = run_command(str(message.content).lower().strip())
+        if response == "" or None: # Checks if the response is None or ""
+            response = "No output" # Then will reply with no output
+        await message.channel.send(f"``` {response} ```")
+        await message.remove_reaction('✅', bot.user)
+
+# Command to check Your id to replace it the OWNER_ID in .env file
+@bot.command()
+@commands.is_owner()
+async def show_id(ctx):
+    await ctx.send(ctx.author.id, ephemeral=True)
+
+# Run the bot
 bot.run(BOT_KEY)
